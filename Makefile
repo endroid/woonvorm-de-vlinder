ee1c0e90347b4039146788676815c7fbdfd293b2
Jeroen van den Enden <info@endroid.nl>
2024-09-09 23:56:55 +0200

    Set up build process

---
+++ Makefile
@@ -0,0 +1,11 @@
+up:
+	@cp --update=none compose.yml.dist compose.yml
+	@docker compose pull
+	@docker compose build
+	@docker compose up -d --force-recreate
+
+down:
+	@docker compose down --remove-orphans
+
+login:
+	@docker compose exec -ti php /bin/bash
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\Yaml\Yaml;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+include 'vendor/autoload.php';
+
+$routing = Yaml::parseFile('config/routing.yaml');
+$twig = new Environment(new FilesystemLoader('templates'));
+
+$contents = [];
+foreach ($routing as $route) {
+    $contents[$route['template']] = $twig->render('pages/'.$route['template'].'.html.twig', [
+        'title' => $route['title'],
+        'routing' => $routing,
+    ]);
+
+    file_put_contents('public/'.$route['template'].'.html', $contents[$route['template']]);
+}
+
+$currentRoute = $routing[$_SERVER['REQUEST_URI'] ?? $_SERVER['argv'][1]];
+
+echo $contents[$currentRoute['template']];

4cc676ec8e52f404037bf6be20791f6f21da150a
Jeroen van den Enden <info@endroid.nl>
2024-09-14 20:06:01 +0200

    Refactoring and add Turbo

---
+++ docs/scripts/app.js
@@ -0,0 +1,3 @@
+import * as Turbo from 'https://cdn.jsdelivr.net/npm/@hotwired/turbo@8.0.4/+esm';
+
+console.log('started');
--- index.php
+++ index.php
@@ -6,8 +6,19 @@ use Twig\Loader\FilesystemLoader;
 
 include 'vendor/autoload.php';
 
-$routing = Yaml::parseFile('config/routing.yaml');
-$twig = new Environment(new FilesystemLoader('templates'));
+$docsPath = __DIR__.'/docs';
+$requestUri = $_SERVER['REQUEST_URI'] ?? $_SERVER['argv'][1];
+
+if (is_file($docsPath.$requestUri)) {
+    $types = ['css' => 'text/css', 'js' => 'text/javascript'];
+    $extension = pathinfo($docsPath.$requestUri, PATHINFO_EXTENSION);
+    header('Content-Type: '.($types[$extension] ?? mime_content_type($docsPath.$requestUri)));
+    readfile($docsPath.$requestUri);
+    die;
+}
+
+$routing = Yaml::parseFile(__DIR__.'/config/routes.yaml');
+$twig = new Environment(new FilesystemLoader(__DIR__.'/templates'));
 
 $contents = [];
 foreach ($routing as $route) {
@@ -16,9 +27,9 @@ foreach ($routing as $route) {
         'routing' => $routing,
     ]);
 
-    file_put_contents('docs/'.$route['template'].'.html', $contents[$route['template']]);
+    file_put_contents(__DIR__.'/docs/'.$route['template'].'.html', $contents[$route['template']]);
 }
 
-$currentRoute = $routing[$_SERVER['REQUEST_URI'] ?? $_SERVER['argv'][1]];
+$currentRoute = $routing[$requestUri];
 
 echo $contents[$currentRoute['template']];
